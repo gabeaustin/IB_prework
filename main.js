3d9d602e470e008411fc40ee7cbfe086fee6cd88
Gabe Austin <gabriel.austin2019@gmail.com>
2020-12-24 19:32:30 -0600

    add test text to js file

---
+++ main.js
@@ -0,0 +1,21 @@
+/*
+Write the song 99 lines of code in a FUNCTION. 
+The function will run 99 lines of codes when the function is called. 
+(You should take into account that when you hit 1 line of code in the file 
+	the word line is NOT plural). 
+*/
+
+/*
+*** Your challenge is to use the coding fundamentals you know or have learned 
+to complete the assignment given. 
+*/
+
+/*
+*** Example Output: 
+99 lines of code in the file, 
+99 lines of code; 
+John strikes one out, clears it all out, 
+98 lines of code in the file, etc…
+*/
+
+document.write("<p>## lines of code in the file, ## lines of code; Gabe strikes one out, clears it all out, ## lines of code in the file...</p>");
