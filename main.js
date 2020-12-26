4b087abd05808507483807e305029dc75976026a
Gabe Austin <gabriel.austin2019@gmail.com>
2020-12-26 00:35:33 -0600

    correct error - remove br tags

--- main.js
+++ main.js
@@ -2,10 +2,10 @@ let lines_of_code = 99;
 
 function check_lines_of_code(){
 	while(lines_of_code >= 2){
-		console.log(lines_of_code + " lines of code in the file, " + lines_of_code + " lines of code; Gabe strikes one out, clears it all out...<br>");
+		console.log(lines_of_code + " lines of code in the file, " + lines_of_code + " lines of code; Gabe strikes one out, clears it all out...");
 		lines_of_code--;
 	}
-	console.log(lines_of_code + " line of code in the file, " + lines_of_code + " line of code; Gabe strikes one out, clears it all out and says, \"That's all folks!\" <br>");
+	console.log(lines_of_code + " line of code in the file, " + lines_of_code + " line of code; Gabe strikes one out, clears it all out and says, \"That's all folks!\" ");
 }
 
 check_lines_of_code();
